570005cdb1ad87c01aa2728274da9a8735308549
Carlos Hernandez <carlosjhr64@gmail.com>
2009-12-21 11:09:06 -0800

    added column calculations

--- gtk2checkboxes/gtk2checkboxes.rb
+++ gtk2checkboxes/gtk2checkboxes.rb
@@ -4,6 +4,7 @@ require 'gtk2applib/gtk2_app_widgets_checkbuttonentry.rb'
 module CheckBoxes
   include Configuration
   WIDGET = {:font=>FONT[:small], :entry_width=>90}.freeze
+  TOTAL = {:title=>"Totals",:scrolled_width=>200,:scrolled_height=>100}.freeze
 
 class Page < Gtk::HBox
   def add_item(vbox, text='', checked=false)
@@ -16,41 +17,95 @@ class Page < Gtk::HBox
     self.destroy
   end
 
-  def initialize(data_file)
+  def parse(vbox)
+    top = nil
+    nothing = true
+    total = 0.0
+    active = 0.0
+    inactive = 0.0
+    vbox.children.each{|child|
+      if child.class == Gtk::HBox then
+        if child.children.last.class == Gtk2App::Entry then
+          text = child.children.last.text.strip
+          top = text if !top
+          checked = child.children.first.active?
+          if text=~/([\+\-\*\/])((\d+)(\.\d+)?)/ then
+            a,d = $1, $2.to_f
+            case a
+              when '+'
+                total += d
+                active += d	if checked
+                inactive += d	if !checked
+              when '-'
+                total -= d
+                active -= d	if checked
+                inactive -= d	if !checked
+              when '*'
+                total *= d
+                active *= d	if checked
+                inactive *= d	if !checked
+              when '/'
+                total /= d
+                active /= d	if checked
+                inactive /= d	if !checked
+            end
+            nothing = false if nothing
+          end
+        end
+      end
+    }
+    if !nothing then
+      message = <<EOT
+#{top}
+Total:\t#{total}
+Active:\t#{active}
+In-active:\t#{inactive}
+EOT
+      @dialogs.quick_message(message,TOTAL)
+    end
+  end
+
+  def initialize(data_file,dialogs)
     super()
     @save = true
-    vbox = nil
+    @dialogs = dialogs
 
     # Button to add a column
-    Gtk2App::Button.new('+', self, WIDGET){
-      vbox = Gtk::VBox.new
+    button1 = Gtk2App::Button.new('+', self, WIDGET){
+      vbox1 = Gtk::VBox.new
       # Button to add a row
-      Gtk2App::Button.new('+', vbox, WIDGET){|value| add_item(value)}.value = vbox
-      self.pack_start(vbox, false, false)
+      button2 = Gtk2App::Button.new('+', vbox1, WIDGET){|value| add_item(value)}
+      button2.signal_connect('button-press-event'){|s,e| parse(s.parent) if e.button == 3 }
+      button2.value = vbox1
+      self.pack_start(vbox1, false, false)
       self.show_all
     }
 
-    vbox = Gtk::VBox.new
-    Gtk2App::Button.new('+', vbox, WIDGET){|value| add_item(value)}.value = vbox
+    vbox2 = Gtk::VBox.new
+    button3 = Gtk2App::Button.new('+', vbox2, WIDGET){|value| add_item(value)}
+    button3.signal_connect('button-press-event'){|s,e| parse(s.parent) if e.button == 3 }
+    button3.value = vbox2
 
     File.open(data_file,'r') { |fh|
       fh.each { |line|
         line.strip!
         if line == '' then
-          self.pack_start(vbox, false, false)
-          vbox = Gtk::VBox.new
-          Gtk2App::Button.new('+', vbox, WIDGET){|value| add_item(value)}.value = vbox
+          self.pack_start(vbox2, false, false)
+          vbox2 = Gtk::VBox.new
+          button4 = Gtk2App::Button.new('+', vbox2, WIDGET){|value| add_item(value)}
+          button4.signal_connect('button-press-event'){|s,e| parse(s.parent) if e.button == 3 }
+          button4.value = vbox2
         else
           checked = false
           if line =~ /\*\s+(\S.*)/ then
             line = $1
             checked = true
           end
-          add_item(vbox,line,checked)
+          add_item(vbox2,line,checked)
         end
       }
     } if File.exist?(data_file)
-    self.pack_start(vbox, false, false)
+    self.pack_start(vbox2, false, false)
 
     self.signal_connect('destroy') {
       File.rename(data_file, data_file+'.bak') if File.exist?(data_file)
---
+++ project.gemspec
@@ -0,0 +1,59 @@
+require 'date'
+require 'find'
+
+project_version = File.expand_path( File.dirname(__FILE__) ).split(/\//).last
+project, version = nil, nil
+if project_version=~/^(\w+)-(\d+\.\d+\.\d+)$/ then
+  project, version = $1, $2
+else
+  raise 'need versioned directory'
+end
+
+spec = Gem::Specification.new do |s|
+  s.name = project
+  s.version = version
+  s.date = Date.today.to_s
+  s.summary = `head -n 1 README.txt`.strip
+  s.email = "carlosjhr64@gmail.com"
+  s.homepage = "http://ruby-gnome-apps.blogspot.com/search/label/CheckBoxes"
+  s.description = `head -n 5 README.txt | tail -n 3`
+  s.has_rdoc = false
+  s.authors = ['carlosjhr64@gmail.com']
+
+  files = []
+  # Rbs
+  Find.find('.'){|fn|
+    if fn=~/\.rb$/ then
+puts fn
+      files.push(fn)
+    end
+  }
+
+  Find.find('./pngs'){|fn|
+    if fn=~/\.png$/ then
+puts fn
+      files.push(fn)
+    end
+  }
+
+  files.push('README.txt')
+
+  s.files = files
+
+  executables = []
+  Find.find('./bin'){|fn|
+    if fn=~/\/gtk2\w+$/ then
+puts fn
+      executables.push(fn.sub(/^.*\//,''))
+    end
+  }
+  s.executables = executables
+  s.default_executable = project
+
+  s.add_dependency('gtk2applib', '~> 4.1.0')
+  s.requirements << 'gtk2'
+
+  s.require_path = '.'
+
+  s.rubyforge_project = project
+end
