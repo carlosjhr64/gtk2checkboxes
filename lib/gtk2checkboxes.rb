4be2325be7d5b1bfaa77fb988af5a9be83ccefca
CarlosJHR64 <carlosjhr64@gmail.com>
2011-05-03 08:15:03 -0700

    lot of reeking, three remaining reeks.

--- lib/gtk2checkboxes.rb
+++ lib/gtk2checkboxes.rb
@@ -4,85 +4,101 @@ require 'find' # Find defined
 # File defined
 # Gtk defined
 
+# Check Boxex
 module Gtk2CheckBoxes
 
+# Da Page
 class Page < Gtk2AppLib::Widgets::HBox # Page defined
   Gtk2AppLib::Widgets.define_composite(:CheckButton,:Entry)
+  SIGNALS = ['clicked', 'button-press-event']
+
+  def button_action(box,signal,*event)
+    case signal
+      when 'clicked' then self.add_item(box)
+      when 'button-press-event' then Page.parse(box) if event.last.button == 3
+    end
+    false
+  end
+
+  def _read(data_file,vbox)
+    fh = File.open(data_file,'r')
+    fh.each do |line|
+      (line.strip! == '')? (vbox = self.last_button) : self.add_item_line(vbox,line)
+    end
+    fh.close
+  end
+
+  def add_item_line(vbox,line)
+    checked = false
+    if line =~ /\*\s+(\S.*)/ then
+      line = $1
+      checked = true
+    end
+    self.add_item(vbox,line,checked)
+  end
+
+  def last_button
+    vbox = Gtk2AppLib::Widgets::VBox.new(self)
+    button = Gtk2AppLib::Widgets::Button.new('+', vbox, Configuration::BUTTON_OPTIONS, *SIGNALS){|*emits| button_action(*emits)}
+    button.is = vbox
+  end
 
   def initialize(data_file,container)
     super(container)
     @save = true
     @changed = false
 
-    button_action = proc {|box,signal,*event|
-      case signal
-        when 'clicked' then self.add_item(box)
-        when 'button-press-event' then self.parse(box) if event.last.button == 3
-      end
-      false
-    }
-
-    signals = ['clicked', 'button-press-event']
-
     # Button to add a column
-    button1 = Gtk2AppLib::Widgets::Button.new('+', self, Configuration::BUTTON_OPTIONS, *signals){
-      vbox1 = Gtk2AppLib::Widgets::VBox.new(self)
-      # Button to add a row
-      button2 = Gtk2AppLib::Widgets::Button.new('+', vbox1, Configuration::BUTTON_OPTIONS, *signals){|*emits| button_action.call(*emits)}
-      button2.is = vbox1
-      self.show_all
-    }
+    Gtk2AppLib::Widgets::Button.new('+', self, Configuration::BUTTON_OPTIONS, *SIGNALS){ self.first_button }
 
-    vbox2 = Gtk2AppLib::Widgets::VBox.new(self)
-    button3 = Gtk2AppLib::Widgets::Button.new('+', vbox2, Configuration::BUTTON_OPTIONS, *signals){|*emits| button_action.call(*emits)}
-    button3.is = vbox2
+    vbox = Gtk2AppLib::Widgets::VBox.new(self)
+    button = Gtk2AppLib::Widgets::Button.new('+', vbox, Configuration::BUTTON_OPTIONS, *SIGNALS){|*emits| button_action(*emits)}
+    button.is = vbox
 
-    File.open(data_file,'r') { |fh|
-      fh.each { |line|
-        line.strip!
-        if line == '' then
-          vbox2 = Gtk2AppLib::Widgets::VBox.new(self)
-          button4 = Gtk2AppLib::Widgets::Button.new('+', vbox2, Configuration::BUTTON_OPTIONS, *signals){|*emits| button_action.call(*emits)}
-          button4.is = vbox2
-        else
-          checked = false
-          if line =~ /\*\s+(\S.*)/ then
-            line = $1
-            checked = true
-          end
-          self.add_item(vbox2,line,checked)
-        end
-      }
-    } if File.exist?(data_file)
+    _read(data_file,vbox) if File.exist?(data_file)
 
-    self.signal_connect('destroy') {
-      if @changed then
-        File.rename(data_file, data_file+'.bak') if File.exist?(data_file)
-        File.open(data_file,'w'){|fh|
-          nl = false
-          self.children.each{|vbox|
-            if vbox.children.length > 1 then
-              count = 0
-              vbox.each { |box|
-                check, entry = box.children
-                if entry && entry.text.strip.length > 0 then
-                  count += 1
-                  if check.active? then
-                    fh.print "*\t"
-                  else
-                    fh.print "\t"
-                  end
-                  fh.puts entry.text
-                end
-              }
-              fh.puts if count > 0
-            end
-          }
-        }
-        File.rename(data_file, data_file+'.bak') if !@save
-      end
-    }
+    self.signal_connect('destroy'){ self.signal_connect_destroy(data_file) }
   end
+
+  def first_button
+    vbox = Gtk2AppLib::Widgets::VBox.new(self)
+    # Button to add a row
+    button = Gtk2AppLib::Widgets::Button.new('+', vbox, Configuration::BUTTON_OPTIONS, *SIGNALS){|*emits| button_action(*emits)}
+    button.is = vbox
+    self.show_all
+  end
+
+  def self._puts_this(check,entry_text,fh)
+    if entry_text =~ /\S/ then
+      (check.active?)?  (fh.print "*\t") : (fh.print "\t")
+      fh.puts entry_text
+    end
+  end
+
+  def self.puts_this(vbox,fh)
+    count = false
+    vbox.each do |box|
+      check,entry = box.children
+      count ||= Page._puts_this(check,entry.text,fh)	if entry
+    end
+    fh.puts if count
+  end
+
+  def _signal_connect_destroy(data_file)
+    fh = File.open(data_file,'w')
+    #nl = false
+    self.children.each{|vbox| Page.puts_this(vbox,fh) if vbox.children.length > 1 }
+    fh.close
+  end
+
+  def signal_connect_destroy(data_file)
+    bak = data_file+'.bak'
+    if @changed || !@save then # not save means delete
+      File.rename(data_file, bak) if File.exist?(data_file)
+      self._signal_connect_destroy(data_file)	if @save
+    end
+  end
+
   def add_item(vbox, text='', checked=false)
     cbe = Gtk2AppLib::Widgets::CheckButtonEntry.new([Configuration::CHECK_OPTIONS,'toggled'], text, vbox, Configuration::ENTRY_OPTIONS,'focus-in-event'){ @changed ||= true; false }
     cbe.checkbutton.active = checked
@@ -95,55 +111,45 @@ class Page < Gtk2AppLib::Widgets::HBox # Page defined
     self.destroy
   end
 
-  def parse(vbox)
-    top = nil
-    nothing = true
-    total = 0.0
-    active = 0.0
-    inactive = 0.0
-    vbox.children.each{|child|
-      if child.kind_of?( Gtk::HBox ) then
-        if child.children.last.kind_of?( Gtk::Entry ) then
-          text = child.children.last.text.strip
-          top = text if !top
-          checked = child.children.first.active?
-          if text=~/([\+\-\*\/])((\d+)(\.\d+)?)/ then
-            a,d = $1, $2.to_f
-            case a
-              when '+'
-                total += d
-                active += d	if checked
-                inactive += d	if !checked
-              when '-'
-                total -= d
-                active -= d	if checked
-                inactive -= d	if !checked
-              when '*'
-                total *= d
-                active *= d	if checked
-                inactive *= d	if !checked
-              when '/'
-                total /= d
-                active /= d	if checked
-                inactive /= d	if !checked
-            end
-            nothing = false if nothing
-          end
-        end
+  def self.___parse(operator,operand,key,counter)
+    counter[:total] = counter[:total].method(operator).call(operand)
+    counter[key] = counter[key].method(operator).call(operand)
+    counter[:nothing] &&= false
+  end
+
+  def self.__parse(text,checked,counter)
+      counter[:top] = text if !counter[:top]
+      if text=~/([\+\-\*\/])((\d+)(\.\d+)?)/ then
+        key = (checked)? :active : :inactive
+        Page.___parse($1,$2.to_f,key,counter)
       end
-    }
-    if !nothing then
-      message = <<EOT
-#{top}
-Total:\t#{total}
-Active:\t#{active}
-Inactive:\t#{inactive}
+  end
+
+  def self._parse(child_children,counter)
+    child_children_last = child_children.last
+    Page.__parse(child_children_last.text.strip,child_children.first.active?,counter) if child_children_last.kind_of?( Gtk::Entry )
+  end
+
+  def self.parse_report(counter)
+    message = <<EOT
+#{counter[:top]}
+Total:\t#{counter[:total]}
+Active:\t#{counter[:active]}
+Inactive:\t#{counter[:inactive]}
 EOT
-      Gtk2AppLib::DIALOGS.quick_message(message,Configuration::TOTAL)
-    end
+    Gtk2AppLib::DIALOGS.quick_message(message,Configuration::TOTAL)
+  end
+
+  def self.parse(vbox)
+    counter = Hash.new(0)
+    counter[:top] = nil
+    counter[:nothing] = true
+    vbox.children.each{|child| Page._parse(child.children,counter) if child.kind_of?( Gtk::HBox ) }
+    Page.parse_report(counter) if !counter[:nothing]
   end
 end
 
+# Da Notebook
 class Notebook < Gtk2AppLib::Widgets::Notebook
 
   def initialize(container)
